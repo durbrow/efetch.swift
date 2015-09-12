96193df032971ebdd3580ac0c20d6968f7d3909e
Kenneth Durbrow <kmdurbrow@me.com>
2015-09-12 12:47:37 -0400

    Created project

---
+++ efetch/efetch/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  efetch
+//
+//  Created by Kenneth Durbrow on 9/12/15.
+//  Copyright © 2015 Kenneth Durbrow. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
