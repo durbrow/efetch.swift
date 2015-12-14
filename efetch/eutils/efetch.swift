40d19bdc35cc3626b0f95b8e951465a667f5e93a
Kenneth Durbrow <kmdurbrow@me.com>
2015-12-13 22:38:35 -0500

    Removed exception Removed "private" to enhance testability Changed overly complicated URL making mechanism Added SRARunList function

--- efetch/eutils/efetch.swift
+++ efetch/eutils/efetch.swift
@@ -9,30 +9,23 @@
 import Foundation
 
 public struct EUtils {
-    private struct URL : CustomStringConvertible {
-        let url: NSURL
-        
-        init(function: String, params: [String:String])
-        {
-            let comp = NSURLComponents()
-            
-            comp.scheme = "http"
-            comp.host = "eutils.ncbi.nlm.nih.gov"
-            comp.path = "/entrez/eutils/\(function).fcgi"
-            comp.queryItems = params.map { NSURLQueryItem(name: $0.0, value: $0.1) }
-            
-            url = comp.URL!
-        }
-        
-        var description: String { get { return url.description } }
-    }
 }
 
-private extension EUtils {
-    static func searchURL(term: String, database: String) -> URL
+extension EUtils {
+    static func URL(function: String, params: [String : String]) -> NSURL
     {
-        return URL(
-            function: "esearch",
+        let comp = NSURLComponents()
+        
+        comp.scheme = "http"
+        comp.host = "eutils.ncbi.nlm.nih.gov"
+        comp.path = "/entrez/eutils/\(function).fcgi"
+        comp.queryItems = params.map { NSURLQueryItem(name: $0.0, value: $0.1) }
+        
+        return comp.URL!
+    }
+    static func searchURL(term: String, database: String) -> NSURL
+    {
+        return URL("esearch",
             params  : [
                 "db"        : database,
                 "retmode"   : "json",
@@ -40,10 +33,9 @@ private extension EUtils {
             ]
         )
     }
-    static func summaryURL(gi: String, database: String) -> URL
+    static func summaryURL(gi: String, database: String) -> NSURL
     {
-        return URL(
-            function: "esummary",
+        return URL("esummary",
             params  : [
                 "db"        : database,
                 "retmode"   : "json",
@@ -51,10 +43,9 @@ private extension EUtils {
             ]
         )
     }
-    static func fetchURL(gi: String, database: String) -> URL
+    static func fetchURL(gi: String, database: String) -> NSURL
     {
-        return URL(
-            function: "efetch",
+        return URL("efetch",
             params  : [
                 "db"        : database,
                 "retmode"   : "text",
@@ -65,10 +56,10 @@ private extension EUtils {
     }
 }
 
-private extension EUtils {
-    static func search(term: String, database: String) -> [String]
+extension EUtils {
+    static func IDList(database: String, term: String) -> [String]
     {
-        let url = searchURL(term, database: database).url
+        let url = searchURL(term, database: database)
         guard
             let data = try? NSData(contentsOfURL: url, options: []),
             let json = try? NSJSONSerialization.JSONObjectWithData(data, options: []),
@@ -81,11 +72,11 @@ private extension EUtils {
 }
 
 public extension EUtils {
-    static func summary(accession: String) throws -> [String:AnyObject]
+    static func summary(database: String, accession: String) -> [String:AnyObject]
     {
-        let gilist = search(accession, database: "nuccore")
+        let gilist = IDList(database, term: accession)
         guard gilist.count > 0 else { return [:] }
-        let url = summaryURL(gilist[0], database: "nuccore").url
+        let url = summaryURL(gilist[0], database: database)
         guard
             let data = try? NSData(contentsOfURL: url, options: []),
             let json = try? NSJSONSerialization.JSONObjectWithData(data, options: []),
@@ -100,11 +91,84 @@ public extension EUtils {
 public extension EUtils {
     static func FASTA(accession: String, f: (String) -> Bool) throws
     {
-        let gilist = search(accession, database: "nuccore")
+        let gilist = IDList("nuccore", term: accession)
         guard gilist.count > 0 else { return }
-        let url = fetchURL(gilist[0], database: "nuccore").url
+        let url = fetchURL(gilist[0], database: "nuccore")
         try String(contentsOfURL: url, encoding: NSUTF8StringEncoding).enumerateLines { (line, inout stop: Bool) in
             stop = f(line)
         }
+    }
+}
+
+func extractRunAttributes(escapedXML: String) -> [EUtils.Run]
+{
+    let xml = escapedXML.stringByReplacingOccurrencesOfString("&lt;", withString: "<")
+                        .stringByReplacingOccurrencesOfString("&gt;", withString: ">")
+                        .stringByReplacingOccurrencesOfString("&amp;", withString: "&")
+
+    class parserDelegate: NSObject, NSXMLParserDelegate {
+        var runs : [EUtils.Run] = []
+        
+        @objc func parser(_: NSXMLParser, didStartElement element: String, namespaceURI: String?, qualifiedName: String?, attributes: [String : String]) {
+            if element == "Run" && attributes["acc"] != nil {
+                runs.append(EUtils.Run(attr: attributes))
+            }
+        }
+    }
+    let d = parserDelegate()
+    let p = NSXMLParser(data: xml.dataUsingEncoding(NSUTF8StringEncoding)!)
+    
+    p.delegate = d
+    if p.parse() {
+        return d.runs
+    }
+    return []
+}
+
+public extension EUtils {
+    public struct Run : CustomStringConvertible {
+        private let attr: [String : String]
+        
+        public var accession: String {
+            get {
+                return attr["acc"]!
+            }
+        }
+        public var baseCount: UInt {
+            get {
+                if let x = attr["total_bases"], y = UInt(x) { return y }
+                return 0
+            }
+        }
+        public var spotCount: UInt {
+            get {
+                if let x = attr["total_spots"], y = UInt(x) { return y }
+                return 0
+            }
+        }
+        public var isLoaded: Bool {
+            get {
+                if let x = attr["load_done"] where x == "true" { return true }
+                return false
+            }
+        }
+        public var isPublic: Bool {
+            get {
+                if let x = attr["is_public"] where x == "true" { return true }
+                return false
+            }
+        }
+        public var description: String { get { return attr.description } }
+    }
+    static func SRARunList(term: String) -> [Run]
+    {
+        var rslt = [Run]()
+        for id in IDList("sra", term: term) {
+            let s = summary("sra", accession: id)
+            if let runs = s["runs"] as? String {
+                rslt.appendContentsOf(extractRunAttributes(runs))
+            }
+        }
+        return rslt
     }
 }
